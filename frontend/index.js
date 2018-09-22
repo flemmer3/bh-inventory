20baabaf7fa412f6a7d35f1716f900b7d14e61e2
Marzuk Rashid <marzukr@gmail.com>
2018-09-21 19:16:15 -0500

    removed .vscode cache folder

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// main app
+//main app
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('app'))

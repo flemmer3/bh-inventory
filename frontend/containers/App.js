0436939fadef210c165fb6ce12cd0cab0fcd4dff
Marzuk Rashid <marzukr@gmail.com>
2018-09-23 20:20:59 -0500

    barebones frontend started

--- frontend/containers/App.js
+++ frontend/containers/App.js
@@ -4,6 +4,27 @@ export default class App extends Component
 {
     render() 
     {
-        return <p>This is my new react app</p>
+        return <div>
+            <table>
+                <tr>
+                    <th>Device ID</th>
+                    <th>Code</th>
+                    <th>User</th>
+                    <th>Notes</th>
+                    <th>Description</th>
+                    <th>Receiver</th>
+                    <th>Value</th>
+                </tr>
+                <tr>
+                    <td>3118009W</td>
+                    <td>5</td>
+                    <td>RESERVED<br/>T32</td>
+                    <td></td>
+                    <td>hp Pavilion dm3</td>
+                    <td></td>
+                    <td>$500</td>
+                </tr>
+            </table>
+        </div>;
     }
 }
--- frontend/webpack/webpack.dev.config.js
+++ frontend/webpack/webpack.dev.config.js
@@ -8,13 +8,37 @@ module.exports = {
         path.join(parentDir, 'index.js')
     ],
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            },{
+            },
+            {
+                test: /\.css$/,
+                loader:['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {loader: 'style-loader'}, // Adds CSS to the DOM by injecting a `<style>` tag
+                    {loader: 'css-loader'}, // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                    {
+                        loader: 'postcss-loader', // Loader for webpack to process CSS with PostCSS
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {loader: 'sass-loader'} // Loads a SASS/SCSS file and compiles it to CSS
+                ]
+            },
+            {
                 test: /\.less$/,
-                loaders: ["style-loader", "css-loder", "less-loader"]
+                loaders: ["style-loader", "css-loader", "less-loader"]
             }
         ]
     },
